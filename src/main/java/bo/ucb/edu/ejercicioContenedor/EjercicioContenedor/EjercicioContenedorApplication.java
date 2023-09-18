e14c9266aa9243a138b7bb276c51ba43407e761d
imferr <117535778+imferr@users.noreply.github.com>
2023-09-17 21:26:43 -0400

    Agregando dependencias

---
+++ src/main/java/bo/ucb/edu/ejercicioContenedor/EjercicioContenedor/EjercicioContenedorApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.ejercicioContenedor.EjercicioContenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioContenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioContenedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/ucb/edu/ejercicioContenedor/EjercicioContenedor/EjercicioContenedorApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.ejercicioContenedor.EjercicioContenedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioContenedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
